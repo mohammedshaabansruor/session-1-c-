ac541d24a88d6add32c0c3766d789153055a55a3
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-19 23:46:02 +0200

    q1

--- session 1/Program.cs
+++ session 1/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            Console.WriteLine("hi");
+            #endregion
+            
         }
     }
 }
